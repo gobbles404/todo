bff9c83d196e0120aa393192ab7df93159050476
gobbles <80227624+gobbles404@users.noreply.github.com>
2023-09-30 19:16:56 -0500

    Set up backend and established MongoDB connection

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const connectDB = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
