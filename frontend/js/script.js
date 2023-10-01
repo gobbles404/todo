a08326a2901bf8b249a58c8fbe1d2db378360550
gobbles <80227624+gobbles404@users.noreply.github.com>
2023-10-01 15:16:11 -0500

    connect frontend to server

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,2 +1,28 @@
-
 console.log('js is working');
+
+// script.js
+document.getElementById('create-todo-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const todoTitle = document.getElementById('todo-input').value;
+  console.log('new todo created', todoTitle);
+
+  // ask Muhib about why localhost works and 127.0.0.1 returns cors error
+  fetch('http://localhost:3001/create', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          title: todoTitle,
+      }),
+  })
+  .then(response => response.json())  // Parse the JSON from the response
+  .then(data => {
+      console.log('Success:', data);  // Log the data received from the server
+  })
+  .catch((error) => {
+      console.error('Error:', error);  // Log any errors
+  });
+});
+
+
--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 
 // feels like python imports
 const express = require('express');
+const morgan = require('morgan');
+
 const connectDB = require('./db');
+const cors = require('cors');
 
 // feels like creating an instance of a class
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 connectDB();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use(cors({
+    origin: 'http://127.0.0.1:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 204,
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -15,4 +27,19 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.post('/create', (req, res) => {
+    console.log('made it in post');
+    try {
+        console.log(req.body);
+        res.json({ message: 'Received the request' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Test endpoint is working' });
 });
